50af0305fd77e2014355be93ed9beeed29704775
etal <etal>
2025-07-10 11:02:16 +0900

    Resolved issue where email verification failed on different devices ver2

--- app/Http/Controllers/VerifyEmailController.php
+++ app/Http/Controllers/VerifyEmailController.php
@@ -2,24 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Log;
-use App\Models\User;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Access\AuthorizationException;
+use App\Models\User;
 
-class VerifyEmailController
+class VerifyEmailController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
-        if (! URL::hasValidSignature($request)) {
-            return response()->json(['message' => '署名が無効または期限切れです。'], 403);
-        }
-
+        // IDとhashからユーザー取得
         $user = User::findOrFail($request->route('id'));
 
-        if (! hash_equals(sha1($user->getEmailForVerification()), $request->route('hash'))) {
-            return response()->json(['message' => 'ハッシュが一致しません。'], 403);
+        if (! hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            throw new AuthorizationException();
         }
 
         if ($user->hasVerifiedEmail()) {
